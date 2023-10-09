8b43ba89aa2414bcc96d562adbcc7d7dba00b00e
Dinar Subeev <dinars163@gmail.com>
2023-10-09 20:51:56 +0400

    SplashScreen

--- app/src/main/java/com/ggr3ml1n/quizz/MainActivity.kt
+++ app/src/main/java/com/ggr3ml1n/quizz/MainActivity.kt
@@ -2,10 +2,12 @@ package com.ggr3ml1n.quizz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val splash = installSplashScreen()
         setContentView(R.layout.activity_main)
     }
 }
